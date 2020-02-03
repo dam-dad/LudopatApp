e4803b637a0a32000019696e76b774c0901ef329
Kevin J. Rodríguez Morales <kjrm97@gmail.com>
2020-02-03 23:51:27 +0000

    TERMINADAS las transiciones entre el menu y la selección de avatares

--- src/main/java/ui/config/PlayerSelectionController.java
+++ src/main/java/ui/config/PlayerSelectionController.java
@@ -7,22 +7,19 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
 import ui.config.avatar.Avatar;
 import ui.config.avatar.AvatarSelector;
 
 /**
  * 
- * <b>Controlador configuración jugadores</b>
- * <br><br>
+ * <b>Controlador configuración jugadores</b> <br>
+ * <br>
  * 
- * Controlador para los ajustes de multijugador con
- * opciones para elegir los distintos personajes 
- * y el nombre de cada jugador
+ * Controlador para los ajustes de multijugador con opciones para elegir los
+ * distintos personajes y el nombre de cada jugador
  * 
  * @author David Fernández Nieves
  * @author Pablo Daniel Urtiaga Pinto
@@ -35,45 +32,87 @@ public class PlayerSelectionController extends AnchorPane implements Initializab
 	@FXML
 	GridPane grid;
 	@FXML
-	ImageView player1Image;
-	
-	//TODO Añadir imagenes y nombres de avatar finales.
-	Avatar[] availableAvatars = {new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 1"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 2"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 3"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 4")
-			, new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 5"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 6"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 7"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 8"), new Avatar(getClass().getResource("/ui/images/Frame.png").toString(),"")};
-	
-	int[] selectedAvatars = {0, 1, 2, 3};
-	
+	StackPane stack;
+
+	String[][] avatarsReferences = { { getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 1" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 2" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 3" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 4" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 5" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 6" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 7" },
+			{ getClass().getResource("/ui/images/Frame.png").toString(), "Avatar 8" }, };
+
+	// TODO Añadir imagenes y nombres de avatar finales.
+	Avatar[] availableAvatars = { new Avatar(avatarsReferences[0][0], avatarsReferences[0][1]),
+			new Avatar(avatarsReferences[1][0], avatarsReferences[1][1]),
+			new Avatar(avatarsReferences[2][0], avatarsReferences[2][1]),
+			new Avatar(avatarsReferences[3][0], avatarsReferences[3][1]),
+			new Avatar(avatarsReferences[4][0], avatarsReferences[4][1]),
+			new Avatar(avatarsReferences[5][0], avatarsReferences[5][1]),
+			new Avatar(avatarsReferences[6][0], avatarsReferences[6][1]),
+			new Avatar(avatarsReferences[7][0], avatarsReferences[7][1]), };
+	int[] selectedAvatars = { 0, 1, 2, 3 };
+
+	AvatarSelector selector;
+
 	public PlayerSelectionController() {
-		
+
 		try {
-			
+
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/fxml/PlayerSelectionView.fxml"));
 			loader.setController(this);
 			loader.setRoot(this);
 			loader.load();
-			
+
+			reloadImages();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private void reloadImages() {
+		grid.add(availableAvatars[selectedAvatars[0]], 0, 0);
+		grid.add(availableAvatars[selectedAvatars[1]], 1, 0);
+		grid.add(availableAvatars[selectedAvatars[2]], 0, 1);
+		grid.add(availableAvatars[selectedAvatars[3]], 1, 1);
+
+		grid.getChildren().get(0).setOnMouseClicked(e -> changeAvatar(0));
+		grid.getChildren().get(1).setOnMouseClicked(e -> changeAvatar(1));
+		grid.getChildren().get(2).setOnMouseClicked(e -> changeAvatar(2));
+		grid.getChildren().get(3).setOnMouseClicked(e -> changeAvatar(3));
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Acciones para las imagenes.
-		player1Image.setOnMouseClicked(e -> changeAvatar(0));
 	}
-	
+
 	private void changeAvatar(int posPlayer) {
-		//TODO Lanzar el JFDialog para la elección del avatar que devolvuelve la posicion del avatar seleccionado
-		
-		//LAS SIGUIENTES LINEAS DE CODIGO SON SOLO DE PRUEBA
-		///*
-		Stage stage = new Stage();
-		AvatarSelector selector = new AvatarSelector(posPlayer, availableAvatars);
-		Scene scene = new Scene(selector);
-		stage.setScene(scene);
-		stage.show();
-		//*/
+		selector = new AvatarSelector(posPlayer, availableAvatars, selectedAvatars);
+
+		stack.getChildren().add(1, selector);
+
+		selector.getFinish().addListener(e -> closeDialog(posPlayer));
+	}
+
+	private void closeDialog(int pos) {
+		selectedAvatars[pos] = selector.getSelected();
+
+		stack.getChildren().remove(1);
+
+		Avatar[] aux = { new Avatar(avatarsReferences[0][0], avatarsReferences[0][1]),
+				new Avatar(avatarsReferences[1][0], avatarsReferences[1][1]),
+				new Avatar(avatarsReferences[2][0], avatarsReferences[2][1]),
+				new Avatar(avatarsReferences[3][0], avatarsReferences[3][1]),
+				new Avatar(avatarsReferences[4][0], avatarsReferences[4][1]),
+				new Avatar(avatarsReferences[5][0], avatarsReferences[5][1]),
+				new Avatar(avatarsReferences[6][0], avatarsReferences[6][1]),
+				new Avatar(avatarsReferences[7][0], avatarsReferences[7][1])
+				};
+		availableAvatars = aux;
+
+		reloadImages();
 	}
 
 }
--- src/main/java/ui/config/avatar/AvatarSelector.java
+++ src/main/java/ui/config/avatar/AvatarSelector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,7 +17,11 @@ public class AvatarSelector extends GridPane implements Initializable {
 	@FXML
 	GridPane grid;
 	
-	public AvatarSelector(int selectedPos, Avatar[] availableAvatars) {
+	int selected;
+	
+	private BooleanProperty finish = new SimpleBooleanProperty(false);
+	
+	public AvatarSelector(int selectedPos, Avatar[] availableAvatars, int[] selectedAvatarsPositions) {
 		try {
 
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/fxml/avatar/AvatarSelector.fxml"));
@@ -23,7 +29,13 @@ public class AvatarSelector extends GridPane implements Initializable {
 			loader.setRoot(this);
 			loader.load();
 			
-			availableAvatars[selectedPos].setSelected(true);
+			this.selected = selectedPos;
+			
+			availableAvatars[selected].setSelected(true);
+			
+			for (int i = 0; i < selectedAvatarsPositions.length; i++) {
+				availableAvatars[selectedAvatarsPositions[i]].setSelected(true);
+			}
 			
 			grid.add(availableAvatars[0], 1, 1);
 			grid.add(availableAvatars[1], 3, 1);
@@ -35,15 +47,37 @@ public class AvatarSelector extends GridPane implements Initializable {
 			grid.add(availableAvatars[7], 7, 3);
 			
 			grid.setAlignment(Pos.CENTER);
+			
+			grid.getChildren().get(0).setOnMouseClicked(e -> selected(0));
+			grid.getChildren().get(1).setOnMouseClicked(e -> selected(1));
+			grid.getChildren().get(2).setOnMouseClicked(e -> selected(2));
+			grid.getChildren().get(3).setOnMouseClicked(e -> selected(3));
+			grid.getChildren().get(4).setOnMouseClicked(e -> selected(4));
+			grid.getChildren().get(5).setOnMouseClicked(e -> selected(5));
+			grid.getChildren().get(6).setOnMouseClicked(e -> selected(6));
+			grid.getChildren().get(7).setOnMouseClicked(e -> selected(7));
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void selected(int selected) {
+		this.selected = selected;
+		this.finish.set(true);
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+	}
+	
+	public int getSelected() {
+		return selected;
+	}
+	
+	public BooleanProperty getFinish() {
+		return this.finish;
 	}
 
 }
