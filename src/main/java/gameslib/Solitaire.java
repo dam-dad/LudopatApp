d58f21025ce3f45d2aa07b10e18cd64913778fcc
Joel Rguez <joloeol@gmail.com>
2020-02-07 11:43:24 +0000

    clases vacias solitario

---
+++ src/main/java/gameslib/Solitaire.java
@@ -0,0 +1,42 @@
+package gameslib;
+
+import java.util.ArrayList;
+
+import games.Card;
+import games.Deck;
+import games.Game;
+import games.GameRules;
+import games.Player;
+
+public class Solitaire extends Game {
+
+	public Solitaire(Deck deck, GameRules gameRules, ArrayList<Player> currentPlayers) {
+		super(deck, gameRules, currentPlayers);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void endGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void throwCard(Card card) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dealCards() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
