fcb42efe9e489c2058d2909f374f3ac771d848b9
pablo23232 <pablo23232@gmail.com>
2020-02-14 12:39:13 +0000

    cambios en la ip

--- src/main/java/uinet/WaitingRoomController.java
+++ src/main/java/uinet/WaitingRoomController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import main.LudopatApp;
+import util.ipSearch;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -96,21 +97,8 @@ public class WaitingRoomController extends AnchorPane implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		String systemipaddress = ""; 
-        try
-        { 
-            URL url_name = new URL("http://bot.whatismyipaddress.com"); 
-  
-            BufferedReader sc = 
-            new BufferedReader(new InputStreamReader(url_name.openStream())); 
-  
-            // reads system IPAddress 
-            systemipaddress = sc.readLine().trim(); 
-        } 
-        catch (Exception e) 
-        { 
-            systemipaddress = "Cannot Execute Properly"; 
-        } 
+		String systemipaddress= ipSearch.ip();
+		
         ipLabel.setText(systemipaddress);
 
 		usersImage.addAll( Arrays.asList(player1Image, player2Image, player3Image, player4Image));
