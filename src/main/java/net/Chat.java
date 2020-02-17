ec84f697d3e780bb0979314e3c0cf479228558dc
Joel Rguez <joloeol@gmail.com>
2020-02-17 09:42:41 +0000

    sad

--- src/main/java/net/Chat.java
+++ src/main/java/net/Chat.java
@@ -61,7 +61,7 @@ public class Chat implements Initializable {
 
 	@FXML
 	void closeChat(ActionEvent event) {
-		dosGame.closeChat();
+		dosGame.getNETHud().closeChat();
 	}
 
 	public Chat(Dos dosGame) {
